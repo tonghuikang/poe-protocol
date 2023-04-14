f22364d2d8cdf580d320b4b300f3c28eae3e9d64
Hui Kang Tong <you@example.com>
2023-04-13 20:27:21 -0700

    update message

--- fastapi_poe/src/fastapi_poe/samples/assets/messages.py
+++ fastapi_poe/src/fastapi_poe/samples/assets/messages.py
@@ -12,33 +12,51 @@ I am querying the language model for analysis ...
 
 """
 
-MULTIPLE_WORDS_FAILURE_REPLY = """\
-Please send a URL to an image or pdf of your resume.
-
+MULTIWORD_FAILURE_REPLY = """\
 Please only send a URL.
 Do not include any other words in your reply.
 
-This is an example
+These are examples of resume the bot can accept.
 
 https://raw.githubusercontent.com/jakegut/resume/master/resume.png
 
-This is another example
-
 https://pjreddie.com/static/Redmon%20Resume.pdf
+
+https://i.imgur.com/XYyvW6B.png
+
+https://media.discordapp.net/attachments/1070915874996895744/1070915875139506296/jake.jpg
+
+See https://poe.com/huikang/1512927999933968 for an example of an interaction.
+
+You can also try https://poe.com/xyzFormatter for advice specifically on your bullet points.
 """
 
 PARSE_FAILURE_REPLY = """
 I could not load your resume.
 Please check whether the link is publicly accessible.
 
-This is an example
+If you are uploading to Github, please ensure that you are sending something like
 
 https://raw.githubusercontent.com/jakegut/resume/master/resume.png
 
-This is another example
+rather than
 
-https://pjreddie.com/static/Redmon%20Resume.pdf
+https://github.com/jakegut/resume/blob/master/resume.png
 
-You can upload your resume to sites like imgur or GitHub.
-Remember to redact sensitive information.
+To get to raw.githubusercontent.com from github.com, right click "Download" and "Copy link address".
+
+
+If you are uploading to imgur, please ensure that you are sending something like
+
+https://i.imgur.com/XYyvW6B.png
+
+rather than
+
+https://imgur.com/a/6AeB8Vy
+
+To get from i.imgur.com from imgur.com, right click the image and "Copy image address".
+
+This bot is not able to accept links from Google drive.
+
+Remember to redact sensitive information, especially contact details.
 """
