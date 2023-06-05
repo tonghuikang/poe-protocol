c4595d17fa60b38e0cd5c03ddd2f958285c1d340
Hui Kang Tong <you@example.com>
2023-06-05 04:33:12 -0700

    update reply strings

--- fastapi_poe/src/fastapi_poe/samples/echo.py
+++ fastapi_poe/src/fastapi_poe/samples/echo.py
@@ -7,7 +7,6 @@ from __future__ import annotations
 
 import asyncio
 import os
-import re
 import tempfile
 from typing import AsyncIterable
 
@@ -17,11 +16,16 @@ from wasmtime import Config, Engine, Linker, Module, Store, WasiConfig
 from fastapi_poe import PoeBot, run
 from fastapi_poe.types import QueryRequest
 
-TESTCASE_FILES = ["test001", "test002"]
+STDIO_PREFIXES = [
+    "./src/fastapi_poe/samples/test001",
+    "./src/fastapi_poe/samples/test002",
+    "./src/fastapi_poe/samples/test003",
+]
+TOTAL_FUEL = 4_000_000_000
 
 
 def run_code(code, stdin_file=None):
-    fuel = 4_000_000_000
+    fuel = TOTAL_FUEL
 
     engine_cfg = Config()
     engine_cfg.consume_fuel = True
@@ -75,21 +79,67 @@ def run_code(code, stdin_file=None):
         )
 
 
-async def execute_code(code):
-    # Redirect stdout temporarily to capture the output of the code snippet
-    captured_output, captured_error = "", ""
+def validate_output(captured_output, expected_output) -> bool:
+    return captured_output.strip() == expected_output.strip()
 
-    loop = asyncio.get_event_loop()
-    (
-        captured_output,
-        captured_error,
-        size,
-        data_len,
-        fuel_consumed,
-    ) = await loop.run_in_executor(
-        None, run_code, code, "./src/fastapi_poe/samples/test003.in"
+
+def format_output(
+    captured_output, captured_error, fuel_consumed, output_correctness
+) -> str:
+    lines = []
+    if output_correctness:
+        line = "Output is CORRECT"
+        lines.append(line)
+    elif fuel_consumed >= TOTAL_FUEL:
+        line = "Time Limit Exceeded"
+        lines.append(line)
+    else:
+        line = "Output is WRONG"
+        lines.append(line)
+
+    if captured_output:
+        line = f"\n```output\n{captured_output}\n```"
+        lines.append(line)
+
+    if captured_error:
+        line = f"\n```error\n{captured_error}\n```"
+        lines.append(line)
+
+    line = f"Fuel consumed: {fuel_consumed:_}"
+    lines.append(line)
+
+    return "\n".join(lines)
+
+
+async def judge_case(code, stdio_prefix=None):
+    stdin_file = stdio_prefix + ".in"
+    expected_stdout_file = stdio_prefix + ".out"
+
+    (captured_output, captured_error, size, data_len, fuel_consumed) = run_code(
+        code, stdin_file
     )
-    return captured_output, captured_error
+
+    with open(expected_stdout_file) as f:
+        expected_output = f.read()
+
+    output_correctness = validate_output(captured_output, expected_output)
+
+    return format_output(
+        captured_output, captured_error, fuel_consumed, output_correctness
+    )
+
+
+async def judge_cases(code):
+    # Redirect stdout temporarily to capture the output of the code snippet
+
+    tasks = [
+        asyncio.create_task(judge_case(code, stdio_prefix))
+        for stdio_prefix in STDIO_PREFIXES
+    ]
+
+    results = await asyncio.gather(*tasks)
+
+    return "\n\n---\n".join(results)
 
 
 def strip_code(code):
@@ -101,33 +151,12 @@ def strip_code(code):
     return code
 
 
-def strip_colors(text):
-    ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
-    return ansi_escape.sub("", text)
-
-
-def wrap_text_in_code_block(text):
-    return "\n```\n" + text + "\n```\n"
-
-
 class EchoBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
         code = query.query[-1].content
         code = strip_code(code)
-        captured_output, captured_error = await execute_code(code)
-
-        if not captured_output and not captured_error:
-            yield self.text_event("No output or error recorded.")
-
-        if captured_output:
-            captured_output = strip_colors(captured_output)
-            captured_output = wrap_text_in_code_block(captured_output)
-            yield self.text_event(captured_output)
-
-        if captured_error:
-            captured_error = strip_colors(captured_error)
-            captured_error = wrap_text_in_code_block(captured_error)
-            yield self.text_event(captured_error)
+        reply_string = await judge_cases(code)
+        yield self.text_event(reply_string)
 
 
 if __name__ == "__main__":
