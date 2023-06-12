ad4d97193194d1d00d6304d125d21a1e97f5860f
Hui Kang Tong <you@example.com>
2023-06-11 17:53:08 -0700

    remove judging part, failed attempt on async

--- fastapi_poe/src/fastapi_poe/samples/echo.py
+++ fastapi_poe/src/fastapi_poe/samples/echo.py
@@ -5,9 +5,9 @@ Sample bot that echoes back messages.
 """
 from __future__ import annotations
 
-import asyncio
 import os
 import tempfile
+import uuid
 from typing import AsyncIterable
 
 from sse_starlette.sse import ServerSentEvent
@@ -21,10 +21,11 @@ STDIO_PREFIXES = [
     "./src/fastapi_poe/samples/test002",
     "./src/fastapi_poe/samples/test003",
 ]
-TOTAL_FUEL = 4_000_000_000
+TOTAL_FUEL = 10_000_000_000
 
 
-def run_code(code, stdin_file=None):
+async def run_code(code, stdin_file=None):
+    # Note: not really async, nice to fix
     fuel = TOTAL_FUEL
 
     engine_cfg = Config()
@@ -41,10 +42,13 @@ def run_code(code, stdin_file=None):
     config.argv = ("python", "-c", code)
     config.preopen_dir(".", "/")
 
+    random_uuid = uuid.uuid4()
+
     with tempfile.TemporaryDirectory() as chroot:
-        out_log = os.path.join(chroot, "out.log")
-        err_log = os.path.join(chroot, "err.log")
-        config.stdin_file = stdin_file
+        out_log = os.path.join(chroot, f"out-{random_uuid}.log")
+        err_log = os.path.join(chroot, f"err-{random_uuid}.log")
+        if stdin_file:
+            config.stdin_file = stdin_file
         config.stdout_file = out_log
         config.stderr_file = err_log
 
@@ -67,6 +71,7 @@ def run_code(code, stdin_file=None):
             with open(err_log) as f:
                 error = f.read()
 
+        # Note: there is no error if the code times out, nice to fix
         with open(out_log) as f:
             result = f.read()
 
@@ -79,23 +84,11 @@ def run_code(code, stdin_file=None):
         )
 
 
-def validate_output(captured_output, expected_output) -> bool:
-    return captured_output.strip() == expected_output.strip()
-
-
-def format_output(
-    captured_output, captured_error, fuel_consumed, output_correctness
-) -> str:
+def format_output(captured_output, captured_error, fuel_consumed) -> str:
     lines = []
-    if output_correctness:
-        line = "Output is CORRECT"
-        lines.append(line)
-    elif fuel_consumed >= TOTAL_FUEL:
-        line = "Time Limit Exceeded"
-        lines.append(line)
-    else:
-        line = "Output is WRONG"
-        lines.append(line)
+
+    if fuel_consumed >= TOTAL_FUEL:
+        lines.append("Time limit exceeded.")
 
     if captured_output:
         line = f"\n```output\n{captured_output}\n```"
@@ -111,37 +104,6 @@ def format_output(
     return "\n".join(lines)
 
 
-async def judge_case(code, stdio_prefix=None):
-    stdin_file = stdio_prefix + ".in"
-    expected_stdout_file = stdio_prefix + ".out"
-
-    (captured_output, captured_error, size, data_len, fuel_consumed) = run_code(
-        code, stdin_file
-    )
-
-    with open(expected_stdout_file) as f:
-        expected_output = f.read()
-
-    output_correctness = validate_output(captured_output, expected_output)
-
-    return format_output(
-        captured_output, captured_error, fuel_consumed, output_correctness
-    )
-
-
-async def judge_cases(code):
-    # Redirect stdout temporarily to capture the output of the code snippet
-
-    tasks = [
-        asyncio.create_task(judge_case(code, stdio_prefix))
-        for stdio_prefix in STDIO_PREFIXES
-    ]
-
-    results = await asyncio.gather(*tasks)
-
-    return "\n\n---\n".join(results)
-
-
 def strip_code(code):
     if len(code.strip()) < 6:
         return code
@@ -155,7 +117,10 @@ class EchoBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
         code = query.query[-1].content
         code = strip_code(code)
-        reply_string = await judge_cases(code)
+        captured_output, captured_error, size, data_len, fuel_consumed = await run_code(
+            code
+        )
+        reply_string = format_output(captured_output, captured_error, fuel_consumed)
         yield self.text_event(reply_string)
 
 
